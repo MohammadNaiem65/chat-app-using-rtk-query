adcda6c26880808a84a4cd04340912c0cecc64b6
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-14 23:41:40 +0600

    set up the header for authenticated request

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,6 +4,15 @@ const apiSlice = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({
 		baseUrl: 'http://localhost:9000',
+		prepareHeaders: async (headers, { getState }) => {
+			const accessToken = getState()?.auth?.accessToken;
+
+			if (accessToken) {
+				headers.set('Authorization', `Bearer ${accessToken}`);
+			}
+
+			return headers;
+		},
 	}),
 	// eslint-disable-next-line no-unused-vars
 	endpoints: (builder) => ({}),
