b2ae81ace6c73b8a984fd5117264a1fce79c71be
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-20 12:36:54 +0600

    set up automated refetch and auto sign out functionality if jwt expired

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,19 +1,36 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { userLoggedOut } from '../auth/authSlice';
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: 'http://localhost:9000',
+	prepareHeaders: async (headers, { getState }) => {
+		const accessToken = getState()?.auth?.accessToken;
+
+		if (accessToken) {
+			headers.set('Authorization', `Bearer ${accessToken}`);
+		}
+
+		return headers;
+	},
+});
 
 const apiSlice = createApi({
 	reducerPath: 'api',
-	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://localhost:9000',
-		prepareHeaders: async (headers, { getState }) => {
-			const accessToken = getState()?.auth?.accessToken;
+	baseQuery: async (args, api, extraOptions) => {
+		const result = await baseQuery(args, api, extraOptions);
 
-			if (accessToken) {
-				headers.set('Authorization', `Bearer ${accessToken}`);
-			}
+		if (
+			result?.error?.status === 401 &&
+			result?.error?.data === 'jwt expired'
+		) {
+			//log out
+			localStorage.removeItem('auth');
 
-			return headers;
-		},
-	}),
+			api.dispatch(userLoggedOut());
+		}
+
+		return result;
+	},
 	// eslint-disable-next-line no-unused-vars
 	endpoints: (builder) => ({}),
 });
